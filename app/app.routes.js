ce72c3dfd34681f34bfa18c1414eb131f4440d20
HappyVincent <vincent.thebault@happytal.com>
2017-06-08 14:41:17 +0200

    Improve routing with clean urls. Add reusable menu and footer.

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,24 +1,61 @@
 var app = angular.module('k2000App', ["ngRoute"]);
 
-app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-
-	$locationProvider.hashPrefix('');
+app.config(function($routeProvider, $locationProvider) {
+	// Need a web server to work properly.
+	$locationProvider.html5Mode(true);
+	$routeProvider.caseInsensitiveMatch = true;
 	$routeProvider
-		.when("/", {
-			templateUrl: "app/components/home/homeView.html",
-			// controller: "homeCtrl"
-		})
 		.when("/home", {
-			templateUrl: "/"
+			templateUrl: "app/components/home/homeView.html",
+			controller: "homeController",
+			controllerAs: "homeCtrl"
+		})
+		.when("/category/:id", {
+			templateUrl: "app/components/category/categoryView.html",
+			controller: "categoryController",
+			controllerAs: "categoryCtrl"
 		})
 		.when("/article", {
-			templateUrl: "/"
+			templateUrl: "app/components/article/articleView.html",
+			controller: "articleController",
+			controllerAs: "articleCtrl"
 		})
 		.otherwise({
-			redirectTo: "/"
+			redirectTo: "/home"
 		});
-}]);
+});
 
-app.controller("homeCtrl", function($scope) {
-	$scope.Message = "Controller speaking from routes.";
+app.controller("indexController", function($route) {
+	this.MenuViewUrl = "app/shared/menu/menuView.html";
+	this.FooterViewUrl = "app/shared/footer/footerView.html";
+
+	var ctrl = this;
+	ctrl.reloadData = function() {
+		$route.reload();
+	}
+});
+
+app.controller("menuController", function() {
+	this.Categories = [
+		{Id: 1, Name: "Weekends"},
+		{Id: 2, Name: "Road Trips"},
+		{Id: 3, Name: "Discoveries"},
+		{Id: 4, Name: "Special Events"}
+	];
+});
+
+app.controller("homeController", function() {
+	var ctrl = this;
+	this.Message = "Home Controller speaking from routes.";
+});
+
+
+
+app.controller("categoryController", function($routeParams) {
+	this.Message = "Category Controller speaking from routes.";
+	this.Id = $routeParams.id;
+});
+
+app.controller("articleController", function() {
+	this.Message = "Article Controller speaking from routes.";
 });
--- app/components/home/homeController.js
+++
@@ -1,5 +0,0 @@
-// var app = angular.module("k2000App", []);
-
-// app.controller("homeCtrl", function($scope) {
-// 	$scope.Message = "Welcome from the controller!";
-// });
